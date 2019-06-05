562ad04edc3fc9cffe93175139219fb768fa36e0
Tom French <tom.french@cantarus.com>
2019-06-05 17:37:30 +0100

    Error handling

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,6 +2,11 @@ import React, {Component} from 'react'
 import QuizQuestionButton from "./QuizQuestionButton";
 
 class QuizQuestion extends Component {
+    constructor(props){
+        super(props);
+        this.state = { incorrectAnswer:false }
+    }
+    
     render(){
         return (
             <main>
@@ -15,9 +20,11 @@ class QuizQuestion extends Component {
                             button_text={answer_option} 
                             key={index}
                             clickHandler={this.handleClick.bind(this)}
-                        />)}
+                        />
+                    )}
                     </ul>
-                </section>¦
+                </section>
+                {this.state.incorrectAnswer? <p className='error'>Sorry, that's not right</p>: null}
             </main>
         );
     }
@@ -25,6 +32,13 @@ class QuizQuestion extends Component {
     handleClick(buttonText){
         if(buttonText === this.props.quiz_question.answer){
             this.props.showNextQuestionHandler();
+            this.setState((state) => {
+                return { incorrectAnswer: false};
+            });
+        } else {
+            this.setState((state) => {
+                return { incorrectAnswer: true};
+            });
         }
     }
 }
