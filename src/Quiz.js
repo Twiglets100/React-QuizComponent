f9cd6aa0c83574bae1b750b061a112ae32d61acb
Tom French <tom.french@cantarus.com>
2019-06-06 09:23:14 +0100

    Reset game.

--- src/Quiz.js
+++ src/Quiz.js
@@ -15,7 +15,9 @@ class Quiz extends Component {
         return (
             <div>
                 {   isQuizEnd
-                    ? <QuizEnd/>
+                    ? <QuizEnd
+                        resetClickHandler={this.handleResetClick.bind(this)}
+                    />
                     : <QuizQuestion 
                         quiz_question={quizData.quiz_questions[this.state.quiz_position-1]} 
                         showNextQuestionHandler={this.showNextQuestion.bind(this)}
@@ -28,6 +30,12 @@ class Quiz extends Component {
     showNextQuestion(){
         this.setState((state) => {
             return {quiz_position: state.quiz_position + 1}
+        })
+    }
+    
+    handleResetClick(){
+        this.setState((state) => {
+            return {quiz_position: 1}
         })
     }
 }
